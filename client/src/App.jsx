9e8f0fcd9e4a2666175c834860f10d745b5ab7ed
Wechuli <wechulipaul@yahoo.com>
2019-01-17 00:02:51 +0300

    building skeletal structure for movies website

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './components/layout/Hero';
+
+
+const App = ()=>{
+  return(
+<React.Fragment>
+  <Hero/>
+</React.Fragment>
+  )
+}
+
+export default App;
---
+++ client/src/components/layout/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <React.Fragment>
+      <section class="hero is-large">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title is-light">All Men Must Die</h1>
+            <h2 class="subtitle">So We eat and drink for tomorrow we die</h2>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Hero;
