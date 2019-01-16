ffe7a2c2c990f9a2a96ea92853763d33f3cc18d5
Wechuli <wechulipaul@yahoo.com>
2019-01-17 00:16:46 +0300

    adding hero and slider

--- client/src/components/layout/Hero.jsx
+++ client/src/components/layout/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <section class="hero is-large">
         <div class="hero-body">
           <div class="container">
-            <h1 class="title is-light">All Men Must Die</h1>
+            <h1 class="title has-centered-text">All Men Must Die</h1>
             <h2 class="subtitle">So We eat and drink for tomorrow we die</h2>
           </div>
         </div>
